8d581c3421daf5688519f2e7b2111762ad8b61ed
Mateusz Żak <matzakoo@gmail.com>
2022-10-18 10:49:24 +0200

    archieving files

--- Client/src/main/java/org/client/javaweb/Main.java
+++ Client/src/main/java/org/client/javaweb/Main.java
@@ -1,15 +1,27 @@
 package org.client.javaweb;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //        Client c = new Client("localhost", 5501);
 //        c.connect();
 //        c.sendMessage();
 //        c.disconnect();
-        FileClient fc = new FileClient("localhost", 5501, "Client/files/");
-        fc.connect();
-        fc.sendFileToServer();
-        fc.getFileFromServer();
-        fc.disconnect();
+
+
+//        FileClient fc = new FileClient("localhost", 5501, "Client/files/");
+//        fc.connect();
+//        fc.sendFileToServer();
+//        fc.getFileFromServer();
+//        fc.disconnect();
+
+        PackageZIP packageZIP = new PackageZIP();
+        File[] files = Paths.get("Client//malpki").toFile().listFiles();
+//        packageZIP.packageArchive(files, "archieve.zip");
+        packageZIP.unpackageArchive(Path.of("Client//filesOut"), "archieve.zip");
     }
 }
---
+++ Client/src/main/java/org/client/javaweb/PackageZIP.java
@@ -0,0 +1,57 @@
+package org.client.javaweb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+public class PackageZIP {
+    private Logger logger = LoggerFactory.getLogger(PackageZIP.class);
+    public void packageArchive(File[] files, String zipName) throws FileNotFoundException {
+        try {
+            ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipName));
+            zipOut.setLevel(9);
+            zipOut.setMethod(ZipOutputStream.DEFLATED);
+            if(files.length > 0){
+                for(File f: files){
+                    ZipEntry zipEntry = new ZipEntry(f.getName());
+                    zipOut.putNextEntry(zipEntry);
+                    FileInputStream fileIn = new FileInputStream(f);
+                    zipOut.write(fileIn.readAllBytes());
+                    fileIn.close();
+                    zipOut.closeEntry();
+                }
+                zipOut.close();
+                logger.info("Archieved: " + zipName);
+            }else{
+                logger.info("Files to archive have not been sent");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void unpackageArchive(Path location, String zipName){
+        try{
+            ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipName));
+            ZipEntry zipEntry;
+            while((zipEntry = zipIn.getNextEntry()) != null){
+                FileOutputStream fileOutputStream = new FileOutputStream(new File(location.toString(), zipEntry.getName()));
+                fileOutputStream.write(zipIn.readAllBytes());
+                fileOutputStream.flush();
+                fileOutputStream.close();
+                zipIn.closeEntry();
+            }
+            zipIn.close();
+            logger.info("Unarchieved: " + zipName);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
